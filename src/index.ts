cfbafc0df0b81af577f0b87b92e21febab4a27dd
Haivex <demozial@gmail.com>
2021-05-15 17:38:16 +0200

    feat: example simple code

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ import product from './product';
 product(2, 5);
 const image = new Image();
 image.src = url;
+
+const b = document.createElement('b');
+b.textContent = 'My text';
+
+document.body.appendChild(b);
